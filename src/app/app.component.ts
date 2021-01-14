cfcbeb847e94674a12d33d1feae0fe09f80c32df
Shokre <kdivus@hotmail.com>
2021-01-14 23:31:21 +0100

    another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blockchain-poll';
+  showForm = false;
+
+  polls = [{
+    question: 'Do you like dogs or cats?',
+    image: 'https://images.pexels.com/photos/1567681/pexels-photo-1567681.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    votes: [0, 1, 5],
+    voted: true,
+  },
+  {
+    question: 'Best month for summer holidays?',
+    image: 'https://images.pexels.com/photos/3601425/pexels-photo-3601425.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    votes: [1, 6, 4],
+    voted: false,
+  }
+]
 }
---
+++ src/app/poll-vote/poll-vote.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import ApexCharts from 'apexcharts';
+
+@Component({
+  selector: 'app-poll-vote',
+  templateUrl: './poll-vote.component.html',
+  styleUrls: ['./poll-vote.component.css']
+})
+export class PollVoteComponent implements OnInit {
+
+  options = ["Monday", "Tuesday", "Wednesday"];
+
+  voteForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.voteForm = this.fb.group({
+      selected: this.fb.control('', [Validators.required]),
+    })
+  }
+  ngOnInit(): void {
+  }
+
+  submitForm() {
+    console.log(this.voteForm.value);
+  }
+
+  generateChart() {
+    
+  }
+}
